8bb388fc152e25eddfacb72ee90563a713d15b22
root <huang.zhou@excelsecu.com>
2021-08-12 17:20:15 +0800

    - transfer tests to c++ implementation with `Catch2` framework

---
+++ projects/TestZByteBuf/tests/mail.cpp
@@ -0,0 +1,10 @@
+//
+//  mail.cpp
+//  normal_test
+//
+//  Created by zhiou on 2021/8/12.
+//  Copyright © 2021 zzstudio. All rights reserved.
+//
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
